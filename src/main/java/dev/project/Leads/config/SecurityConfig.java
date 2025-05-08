b1e81b6ad73b5cfd73e8ebe14240b5eee9515590
mateusDbarros <mb685212@gmail.com>
2025-05-08 06:46:38 -0300

    servidor rodando

---
+++ src/main/java/dev/project/Leads/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package dev.project.Leads.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    // Libera acesso a todas as rotas
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+    // Bean do PasswordEncoder que você já usa
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
